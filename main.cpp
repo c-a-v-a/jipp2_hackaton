e58b1e5acf812bea6a27bc00fed9cc8a6c6312e3
cava <cavaevilmail@gmail.com>
2024-03-26 14:03:08 +0100

    tools

---
+++ main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <locale.h>
+
+#include "tools.h"
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_CTYPE, "Polish");
+
+    if (argc < 2) {
+        // dialy by default
+        return 0;
+    }
+    if (argc > 2) {
+        cout << "Niepoprawne flagi" << endl;
+        print_help();
+        return 0;
+    }
+
+    int mode = parse_args(argv);
+
+    switch(mode) {
+        case Daily:
+            // print dialy
+            return 0;
+        case Random:
+            // print random
+            return 0;
+        case Help:
+            print_help();
+            return 0;
+        case Error:
+            cout << "Niepoprawna flaga" << endl;
+            print_help();
+            return 0;
+    }
+
+    std::cout << "Hello World!\n";
+}
+
---
+++ tools.h
@@ -0,0 +1,37 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+enum ProgramMode {
+    Daily,
+    Random,
+    Help,
+    Error
+};
+
+int parse_args(char *argv[]) {
+    string arg = argv[1];
+
+    if (arg == "-d" || arg == "--daily")
+        return Daily;
+    if (arg == "-r" || arg == "--random")
+        return Random;
+    if (arg == "-h" || arg == "--help")
+        return Help;
+
+    return Error;
+}
+
+void print_help() {
+    cout << "Iam" << endl;
+    cout << "program do codziennych affirmacji" << endl;
+    cout << "flagi:" << endl;
+    cout << "\t-r/--random - wypisz losową afirmację" << endl;
+    cout << "\t-d/--dialy - wypisz afirmację na disiaj" << endl;
+}
+
+#endif
